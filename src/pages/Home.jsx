60683adf086dbb8e37fd24e1165ff237f083ef2f
vinay1723 <vinaykumar.17july@gmail.com>
2026-02-03 21:42:57 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./Home.css"; // add this css file
+
+export default function Home() {
+  const [content, setContent] = useState("");
+  const [ttl, setTtl] = useState("");
+  const [views, setViews] = useState("");
+  const [url, setUrl] = useState("");
+
+  const createPaste = async () => {
+    const res = await fetch("http://localhost:3000/api/pastes", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        content,
+        ttl_seconds: ttl ? Number(ttl) : undefined,
+        max_views: views ? Number(views) : undefined,
+      }),
+    });
+
+    const data = await res.json();
+    setUrl(data.url);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">New Paste</h1>
+
+      <textarea
+        className="editor"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="Paste your text here..."
+      />
+
+      <div className="settings">
+        <h3>Optional Paste Settings</h3>
+
+        <div className="row">
+          <input
+            className="input"
+            placeholder="TTL seconds"
+            value={ttl}
+            onChange={(e) => setTtl(e.target.value)}
+          />
+
+          <input
+            className="input"
+            placeholder="Max views"
+            value={views}
+            onChange={(e) => setViews(e.target.value)}
+          />
+        </div>
+
+        <button className="btn" onClick={createPaste}>
+          Create Paste
+        </button>
+      </div>
+
+      {url && (
+        <p className="link">
+          Share Link: <a href={url}>{url}</a>
+        </p>
+      )}
+    </div>
+  );
+}
---
+++ src/pages/ViewPaste.jsx
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./ViewPaste.css"; // add css file
+
+export default function ViewPaste() {
+  const { id } = useParams();
+  const [content, setContent] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/pastes/${id}`)
+      .then((res) => {
+        if (!res.ok) throw new Error();
+        return res.json();
+      })
+      .then((data) => setContent(data.content))
+      .catch(() => setError("Paste not found or expired"));
+  }, [id]);
+
+  if (error) return <h2 className="error">{error}</h2>;
+
+  return (
+    <div className="view-container">
+      <h1 className="view-title">View Paste</h1>
+
+      <div className="paste-box">
+        <pre>{content}</pre>
+      </div>
+    </div>
+  );
+}
